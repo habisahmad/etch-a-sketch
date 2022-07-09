671060727f0bd30fb285297fa60e2c4aaeaa1036
habisahmad <habis.ahmad123@gmail.com>
2022-07-09 17:13:25 -0400

    grid created with flexbox, loop created to create child divs inside of grid div

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const grid = document.querySelector('#grid');
-//const content = document.createElement('div');
+
+for(let i = 0; i < 16*16; i++){
+    const content = document.createElement('div');
+    content.classList.add('grid');
+    content.style.height = '31.25px'
+    content.style.width = '31.25px'
+    content.style.border = '0.5px solid red'
+    content.style.display = 'grid'
+    grid.appendChild(content)
+}
+
+
+function getCoordinates(event){
+    var bounds = event.target.getBoundingClientRect();
+    let x = event.clientX - bounds.left;
+    let y = event.clientY - bounds.top;
+} 
 
 grid.addEventListener('mouseover', function(e) {
-    console.log(e)
-    //for(let i = 0; i < 16; i++){
-        const content = document.createElement('div');
-        content.classList.add('grid');
-        content.style.height = '31.25px'
-        content.style.width = '31.25px'
-        content.style.border = '2px solid red'
-        content.style.display = 'flex'
-        content.style.flexDirection = 'row'
-        grid.appendChild(content)
+    e.target.style.background = 'blue'
+
 })
 
-content.addEventListener('mouseover', function(e){
-    e.target.style.background = 'red'
-})
+
