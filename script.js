5452fe3d07c4b893cae7771a6635c0db28e4473e
habisahmad <habis.ahmad123@gmail.com>
2022-07-13 22:42:24 -0400

    hex color selector created, user can now choose the color for the etch-a-sketch

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ for(let i = 0; i < 16*16; i++){
     const content = document.createElement('div');
     content.classList.add('grid');
     content.style.border = '0.5px solid red'
-
     grid.appendChild(content)
 }
 
+const colorpicker = document.querySelector('#color');
+const hexInput = document.querySelector('#hex')
 
-grid.addEventListener('mouseover', function(e) {
-    e.target.style.background = 'blue'
+let isTrue = false;
 
+if(isTrue == false){ 
+    grid.addEventListener('mouseover', function(e) {
+        e.target.style.background = 'black'; 
+    })  
+} // This if statement is used to make black the standard for when the user does not input a color.
+
+colorpicker.addEventListener('input', () =>{
+    let color = colorpicker.value
+    hexInput.value = color;
+    isTrue = true;
+    grid.addEventListener('mouseover', function(e) {
+        e.target.style.background = color; 
+    })   
 })
-
-
