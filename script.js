4fcd36dc3515469f31d2e36751670d14645d8698
habisahmad <habis.ahmad123@gmail.com>
2022-07-13 23:32:52 -0400

    clear grid button added

--- script.js
+++ script.js
@@ -9,20 +9,28 @@ for(let i = 0; i < 16*16; i++){
 
 const colorpicker = document.querySelector('#color');
 const hexInput = document.querySelector('#hex')
+const clearButton = document.querySelector('#clear');
 
 let isTrue = false;
 
 if(isTrue == false){ 
     grid.addEventListener('mouseover', function(e) {
         e.target.style.background = 'black'; 
+        clearButton.addEventListener('click', () =>{
+            e.target.style.background = 'white'
+        })
     })  
 } // This if statement is used to make black the standard for when the user does not input a color.
-
 colorpicker.addEventListener('input', () =>{
     let color = colorpicker.value
     hexInput.value = color;
     isTrue = true;
     grid.addEventListener('mouseover', function(e) {
         e.target.style.background = color; 
+        clearButton.addEventListener('click', () =>{
+            e.target.style.background = 'white'
+        })
     })   
 })
+
+
